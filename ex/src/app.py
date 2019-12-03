6dde9700548e80aef810f6283ebd7ba492fa5114
Chris <credd@dbox10.a.net>
2019-12-03 18:18:14 -0500

    updated app.py and docker compose layout options

---
+++ ex/src/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+DPORT = os.environ.get('DPORT')
+
+@app.route('/')
+def hello():
+    return "Hello from ex!"
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',port=DPORT)
